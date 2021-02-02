817a4a58388cf0f661b35a92c0b4bcc9da84fefe
Zach-Fisherman <zacharie.poissonnier@gmail.com>
2021-02-02 15:58:53 +0100

    second exercices

--- my-vue-app/src/components/HelloWorld.vue
+++ my-vue-app/src/components/HelloWorld.vue
@@ -1,50 +1,34 @@
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <p>
-    Recommended setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >Vetur</a>
-    +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=znck.vue-language-features"
-      target="_blank"
-    >Vue DX</a>
-  </p>
-  <p>
-    Make sure to use workspace version of TypeScript to get improved support via
-    <a
-      href="https://github.com/znck/vue-developer-experience"
-      target="_blank"
-    >@vuedx</a>.
-    <br />Note @vuedx is still experimental and this setup is provided for early feedback.
-  </p>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div>
+    <span v-if="bool">{{message}}</span>
+    <span v-if="!bool">{{tableau}}</span>
+    <button @click="changerBool">Changer d'affichage</button>
+  </div>
+  <input-Exo/>
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue'
+import inputExo from './input.vue'
+import texteExo from './texte.vue'
+import listeExo from './texte.vue'
 
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: {
-      type: String,
-      required: true
+  name: 'Donnees',
+  data(){
+  return{
+    message: 'Mon message',
+    bool: true,
+    tableau: ['Clavier','Souris','Trousse']
+  }
+  },
+  methods: {
+    changerBool(){
+      this.bool = !this.bool;
     }
+  },
+  components : {
+    inputExo, texteExo, listeExo,
   },
   setup: () => {
     const count = ref(0)
@@ -55,6 +39,6 @@ export default defineComponent({
 
 <style scoped>
 a {
-  color: #42b983;
+  color: #00ff8c;
 }
 </style>
---
+++ my-vue-app/src/components/input.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="text" v-model="messageVide" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'input',
+  data(){
+    return{
+        messageVide: '',
+    }
+  },
+  components: {
+  }
+})
+</script>
